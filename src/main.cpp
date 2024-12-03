673b7ef88d2b52fd212c5b488c0202b15289378e
Spidious <lukedeffenbaugh@gmail.com>
2024-12-02 21:32:23 -0600

    First Functional State
    message is sent through serial connection successfully

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+/*
+
+A message is sent through the serial bus.
+This message is formatted as such.
+
+hex (32bit)
+aa bc cd ee
+
+section a: START SIGNAL
+    8 bit long section, starting signal will always be 0xC2
+
+section b: FLAGS
+    4 bit long section, each bit has a specific meaning
+
+section c: BUTTONS
+    8 bit long section, each bit represents a button
+    This arrangement allows any number of the 8 available buttons to be pressed at the same time.
+
+section d: EXTRA CALLS
+    4 bit long section, read as an integer 0-15
+    This section will indicate extra functionality outside of the buttons.
+    Only one extra call can be initiated at a time.
+
+section e: ENDING SIGNAL
+    8 bit long section, ending signal will always be 0x43
+
+*/
+
+
+#include <Arduino.h>
+
+// time in ms to wait before checking next button press
+#define DELAY 100
+
+const uint8_t BUTTON_PINS[] = {D1, D2, D5, D6, D7}; // digital pins (maximum of 8 can be used with this code)
+uint32_t msg = 0; // message sent to pc
+
+void setup() {
+  Serial.begin(9600);
+
+  // Set each pin to pullup
+  for (int i = 0; i < 5; i++) {
+    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+  }
+
+  // Set LED pinmode
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  msg = 0x0;  // Clear the message
+
+  // Construct pin section
+  for (int i = 0; i < 5; i++) {
+    if (!digitalRead(BUTTON_PINS[i])) {
+      msg |= 1 << (19-i); // ---- ---- 0000 xxxx xxxx 0000 ---- ----
+    }
+  }
+
+  // Construct System Call section
+  msg |= 0b0000 << 8; // ---- ---- 0000 0000 0000 xxxx ---- ----
+
+  // Construct Flag section
+  msg |= 0b0000 << 20; //  ---- ---- xxxx 0000 0000 0000 ---- ----
+
+  // construct start and finish signals
+  msg |= 0xC2000043;
+
+  // Send the message to serial if it is not an empty message
+  if (msg != 0xC2000043) {
+    // Send start signal
+    Serial.write(msg >> 24); // xxxx xxxx ---- ---- ---- ---- 0000 0000
+
+    // Send the message
+    Serial.write(msg >> 16 & 0xFF);
+    Serial.write(msg >> 8 & 0xFF);
+
+    // Send finish signal
+    Serial.write(msg & 0xFF); // 0000 0000 ---- ---- ---- ---- xxxx xxxx
+  }
+  delay(DELAY); // Delay for a short time
+}
